d7f40e84649c382609479ad0178911d0b9ee5956
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2017-01-21 15:33:44 -0500

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -6,13 +6,13 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '408721156127597', // your App ID
 		'clientSecret' 	: 'fcef4efb49700042ea48897a5c93f6bc', // your App Secret
-		'callbackURL' 	: 'http://appfinity.us/auth/facebook/callback'
+		'callbackURL' 	: 'https://appfinity.us/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
 		'consumerKey' 		: 'xHT1DeCEzZ1rz6OWadVuxHltX',
 		'consumerSecret' 	: 'a5gJG5tNFDYmVk0TtFenGeOzA0vCUo20XqbfCgGeITUcaObu5k',
-		'callbackURL' 		: 'http://appfinity.us/auth/twitter/callback'
+		'callbackURL' 		: 'https://appfinity.us/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
